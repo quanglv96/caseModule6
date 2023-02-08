d9cf98e64e5e263e52c428768e72ebbd25e6f950
quanglv96 <quanglv1712@gmail.com>
2023-02-08 17:02:35 +0700

    add new project

---
+++ BE_NDTQ/src/main/java/com/example/be_ndtq/BeNdtqApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_ndtq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeNdtqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeNdtqApplication.class, args);
+    }
+
+}
